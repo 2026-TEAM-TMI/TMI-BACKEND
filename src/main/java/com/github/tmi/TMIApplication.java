87ae9ed15d98ac0e83d0089ad9bd3b971ff2d238
seahee <lollol0617@gmail.com>
2026-06-29 11:09:08 +0900

    [init] #0 - 스프링부트 애플리케이션 생성

---
+++ src/main/java/com/github/tmi/TMIApplication.java
@@ -0,0 +1,12 @@
+package com.github.tmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TMIApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TMIApplication.class, args);
+	}
+
+}
