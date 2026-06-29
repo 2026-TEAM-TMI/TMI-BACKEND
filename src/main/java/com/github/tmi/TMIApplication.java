dbd71bca069e2a652356a7ab684c1c0dfe87518e
seahee <lollol0617@gmail.com>
2026-06-29 14:05:55 +0900

    [feat] #1 - gradle feign 의존성 추가, 애플리케이션 enable feign client

--- src/main/java/com/github/tmi/TMIApplication.java
+++ src/main/java/com/github/tmi/TMIApplication.java
@@ -2,8 +2,10 @@ package com.github.tmi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TMIApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TMIApplication.class, args);
