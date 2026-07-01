3aaee8bef5db859cb452fc41230dab243bbf9191
seahee <lollol0617@gmail.com>
2026-07-01 11:37:18 +0900

    [feat] #8 - refresh token을 사용해 access token을 재발급하는 로직 구현

--- src/main/java/com/github/tmi/member/service/MemberTokenService.java
+++ src/main/java/com/github/tmi/member/service/MemberTokenService.java
@@ -1,11 +1,18 @@
 package com.github.tmi.member.service;
 
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import com.github.tmi.global.auth.jwt.JwtTokenProvider;
+import com.github.tmi.global.auth.jwt.JwtValidationType;
 import com.github.tmi.global.auth.jwt.TokenRepository;
 import com.github.tmi.global.auth.jwt.exception.TokenErrorCode;
 import com.github.tmi.global.auth.redis.Token;
+import com.github.tmi.global.auth.security.MemberAuthentication;
 import com.github.tmi.global.exception.TMIException;
+import com.github.tmi.member.domain.enums.Role;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 public class MemberTokenService {
 
 	private final TokenRepository tokenRepository;
+	private final JwtTokenProvider jwtTokenProvider;
 
 	@Transactional
 	public void saveRefreshToken(final Long memberId, final String refreshToken) {
@@ -36,6 +44,37 @@ public class MemberTokenService {
 		return token.getId();
 	}
 
+	public String reissueAccessToken(final String refreshToken) {
+		validateRefreshToken(refreshToken);
+
+		Long memberIdFromToken = jwtTokenProvider.getMemberIdFromJwt(refreshToken);
+		Long storedMemberId = findIdByRefreshToken(refreshToken);
+		if (!memberIdFromToken.equals(storedMemberId)) {
+			throw new TMIException(TokenErrorCode.REFRESH_TOKEN_MEMBER_ID_MISMATCH_ERROR);
+		}
+
+		Role role = jwtTokenProvider.getRoleFromJwt(refreshToken);
+		Authentication authentication = new MemberAuthentication(
+			memberIdFromToken,
+			null,
+			List.of(role.toGrantedAuthority())
+		);
+		return jwtTokenProvider.issueAccessToken(authentication);
+	}
+
+	private void validateRefreshToken(final String refreshToken) {
+		JwtValidationType type = jwtTokenProvider.validateToken(refreshToken);
+		switch (type) {
+			case VALID_JWT -> {
+			}
+			case EXPIRED_JWT_TOKEN -> throw new TMIException(TokenErrorCode.REFRESH_TOKEN_EXPIRED_ERROR);
+			case INVALID_JWT_SIGNATURE -> throw new TMIException(TokenErrorCode.REFRESH_TOKEN_SIGNATURE_ERROR);
+			case UNSUPPORTED_JWT_TOKEN -> throw new TMIException(TokenErrorCode.UNSUPPORTED_REFRESH_TOKEN_ERROR);
+			case EMPTY_JWT -> throw new TMIException(TokenErrorCode.REFRESH_TOKEN_EMPTY_ERROR);
+			default -> throw new TMIException(TokenErrorCode.INVALID_REFRESH_TOKEN_ERROR);
+		}
+	}
+
 	@Transactional
 	public void deleteRefreshToken(final Long memberId) {
 		log.info("Deleting refresh token for memberId: {}", memberId);
@@ -48,4 +87,3 @@ public class MemberTokenService {
 		log.info("Successfully deleted refresh token for memberId: {}", memberId);
 	}
 }
-
