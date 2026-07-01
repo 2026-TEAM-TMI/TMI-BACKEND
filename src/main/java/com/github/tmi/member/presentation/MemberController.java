d44d4fe0f8e4d3fa610d8664c67313c75933ab0b
seahee <lollol0617@gmail.com>
2026-07-01 10:55:19 +0900

    [feat] #5 - 사용자의 정보를 반환 API 구현 (controller단, service단)

--- src/main/java/com/github/tmi/member/presentation/MemberController.java
+++ src/main/java/com/github/tmi/member/presentation/MemberController.java
@@ -3,17 +3,21 @@ package com.github.tmi.member.presentation;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.tmi.global.auth.annotation.CurrentMember;
 import com.github.tmi.global.auth.dto.MemberSocialLoginRequest;
 import com.github.tmi.global.response.dto.SuccessResponse;
 import com.github.tmi.member.dto.LoginSuccessResponse;
+import com.github.tmi.member.dto.MemberInfoResponse;
 import com.github.tmi.member.exception.MemberSuccessCode;
 import com.github.tmi.member.service.MemberLoginService;
+import com.github.tmi.member.service.MemberService;
 import com.github.tmi.member.service.MemberTokenService;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -29,6 +33,7 @@ public class MemberController {
 
 	private final MemberLoginService memberLoginService;
 	private final MemberTokenService memberTokenService;
+	private final MemberService memberService;
 
 	@PostMapping("/login")
 	public ResponseEntity<SuccessResponse<LoginSuccessResponse>> login(
@@ -49,5 +54,13 @@ public class MemberController {
 		return ResponseEntity.ok()
 			.header(HttpHeaders.SET_COOKIE, cookie.toString())
 			.body(SuccessResponse.of(MemberSuccessCode.LOGIN_SUCCESS, successResponse));
+	}
+
+	@GetMapping("/me")
+	public ResponseEntity<SuccessResponse<MemberInfoResponse>> getMyInfo(
+		@CurrentMember final Long memberId
+	) {
+		MemberInfoResponse response = memberService.getMyInfo(memberId);
+		return ResponseEntity.ok(SuccessResponse.of(MemberSuccessCode.GET_MY_INFO_SUCCESS, response));
 	}
 }
