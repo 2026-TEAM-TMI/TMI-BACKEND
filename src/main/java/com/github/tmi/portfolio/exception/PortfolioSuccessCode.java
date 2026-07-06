d20bf8e4b18dfae84ef55ab35375cadcc927f8c2
seahee <lollol0617@gmail.com>
2026-07-06 10:09:19 +0900

    [feat] #20 - 포트폴리오 생성 success code 작성

---
+++ src/main/java/com/github/tmi/portfolio/exception/PortfolioSuccessCode.java
@@ -0,0 +1,22 @@
+package com.github.tmi.portfolio.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.github.tmi.global.response.base.BaseCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum PortfolioSuccessCode implements BaseCode {
+
+	/*
+	200 OK
+	 */
+	GENERATE_SUCCESS(HttpStatus.OK, "포트폴리오 생성 성공"),
+	;
+
+	private final HttpStatus httpStatus;
+	private final String message;
+}
