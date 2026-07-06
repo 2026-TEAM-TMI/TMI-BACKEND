29c131acfe1e9c0d34d4543898d3489730d80c50
seahee <lollol0617@gmail.com>
2026-07-06 15:34:16 +0900

    [feat] #22 - 포트폴리오 목록 조회 repository, service단 구현

--- src/main/java/com/github/tmi/portfolio/repository/PortfolioRepository.java
+++ src/main/java/com/github/tmi/portfolio/repository/PortfolioRepository.java
@@ -1,5 +1,7 @@
 package com.github.tmi.portfolio.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.github.tmi.portfolio.domain.Portfolio;
 
 @Repository
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+	List<Portfolio> findAllByMemberIdAndPublished(Long memberId, boolean published);
 }
