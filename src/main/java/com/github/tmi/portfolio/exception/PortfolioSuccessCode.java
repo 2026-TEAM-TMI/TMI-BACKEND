b75016eeb1eb8d828df25f541487c5f81c415559
seahee <lollol0617@gmail.com>
2026-07-07 11:00:51 +0900

    [feat] #23 - 포트폴리오 피드 조회 controller단 로직 작성, success code 생성

--- src/main/java/com/github/tmi/portfolio/exception/PortfolioSuccessCode.java
+++ src/main/java/com/github/tmi/portfolio/exception/PortfolioSuccessCode.java
@@ -15,7 +15,8 @@ public enum PortfolioSuccessCode implements BaseCode {
 	200 OK
 	 */
 	GENERATE_SUCCESS(HttpStatus.OK, "포트폴리오 생성 성공"),
-	GET_PORTFOLIOS_SUCCESS(HttpStatus.OK, "포트폴리오 목록 조회 성공");
+	GET_PORTFOLIOS_SUCCESS(HttpStatus.OK, "포트폴리오 목록 조회 성공"),
+	GET_FEED_SUCCESS(HttpStatus.OK, "포트폴리오 피드 조회 성공");
 
 	private final HttpStatus httpStatus;
 	private final String message;
